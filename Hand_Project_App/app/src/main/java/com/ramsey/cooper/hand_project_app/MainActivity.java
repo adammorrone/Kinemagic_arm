12e0735139e2ee3e3e301627d064f978799760a6
Cooper Ramsey <cooperdramsey@gmail.com>
2017-10-11 18:40:33 -0400

    Add Hand Project app to github repo

---
+++ Hand_Project_App/app/src/main/java/com/ramsey/cooper/hand_project_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ramsey.cooper.hand_project_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
