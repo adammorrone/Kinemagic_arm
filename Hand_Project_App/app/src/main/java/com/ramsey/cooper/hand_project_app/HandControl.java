a4d001471b2a53fc63d716ad51222f4609a501c1
Cooper Ramsey <cooperdramsey@gmail.com>
2017-10-18 14:23:20 -0400

    Add Bluetooth code, and add HandControl layout.

---
+++ Hand_Project_App/app/src/main/java/com/ramsey/cooper/hand_project_app/HandControl.java
@@ -0,0 +1,13 @@
+package com.ramsey.cooper.hand_project_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HandControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hand_control);
+    }
+}
