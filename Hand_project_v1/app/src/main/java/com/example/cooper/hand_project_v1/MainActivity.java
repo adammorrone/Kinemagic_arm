05649a630cb0f25e8309b7f7b2f39af6af14c9ed
Cooper Ramsey <32044877+cooperdramsey@users.noreply.github.com>
2017-11-08 15:08:31 -0500

    Create first activity to show paired bluetooth devices.

--- Hand_project_v1/app/src/main/java/com/example/cooper/hand_project_v1/MainActivity.java
+++ Hand_project_v1/app/src/main/java/com/example/cooper/hand_project_v1/MainActivity.java
@@ -1,13 +1,92 @@
 package com.example.cooper.hand_project_v1;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Connection status text views
+    TextView textConnectionStatus;
+    ListView pairedListView;
+
+    //Bluetooth member fields
+    private BluetoothAdapter mBtAdapter;
+    private ArrayAdapter<String> mPairedDevicesArrayAdapter;
+
+    //Method to easily display toasts.
+    public void Msg(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textConnectionStatus = (TextView) findViewById(R.id.connecting);
+        textConnectionStatus.setTextSize(40);
+
+        //Initialize array adapter for paired devices
+        mPairedDevicesArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+
+        //Find and set up the Listview for paired devices
+        pairedListView = (ListView) findViewById(R.id.paired_devices);
+        pairedListView.setAdapter(mPairedDevicesArrayAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        checkBTState();
+
+        mPairedDevicesArrayAdapter.clear(); //Clear array adapter so items are not duplicated
+
+        textConnectionStatus.setText(" ");
+
+        //Get the local Bluetooth adapter
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        //Get a set of currently paired devices and append it to paired devices list
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+
+        //Add previously paired devices to the array
+        if(pairedDevices.size() > 0) {
+            findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);
+            for (BluetoothDevice device : pairedDevices) {
+                mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        } else {
+            mPairedDevicesArrayAdapter.add("No devices paired.");
+        }
+    }
+
+    //method to check if the device has Bluetooth and if it is on.
+    //Also prompts user to turn it back on
+    private void checkBTState()
+    {
+        // Check device has Bluetooth and that it is turned on
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(mBtAdapter == null) {
+            Msg("Device does not support Bluetooth.");
+            finish();
+        } else {
+            if(!mBtAdapter.isEnabled()) {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
     }
 }
